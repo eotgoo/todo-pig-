39f85520d8ea022f9c944423bf7da67ed3d98443
Gankhulug02 <huluguu0202@gmail.com>
2023-12-16 00:26:49 +0800

    hi untaach

---
+++ fe/app/christmas/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const Christmas = () => {
+  const [word, setWord] = useState("");
+  const [clientWord, setClientWord] = useState("");
+  const [isFunctionWorking, setIsFunctionWorking] = useState(false);
+
+  const alphabet_array = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    " ",
+  ];
+
+  const findAlphabet = ({ word }: any) => {
+    const wordArr = word.split("");
+    const returningWordArr = [""];
+    let delay = 100;
+
+    wordArr.map((wLetter: any) => {
+      alphabet_array.map((aLetter) => {
+        returningWordArr.push(aLetter);
+        if (wLetter.toLowerCase() === aLetter.toLowerCase()) {
+          returningWordArr.push(aLetter);
+        }
+        delay += 50;
+
+        const returningWord = returningWordArr.join("");
+        setTimeout(() => {
+          setWord(returningWord);
+        }, delay);
+        returningWordArr.pop();
+      });
+    });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center  h-screen">
+      {isFunctionWorking === true ? (
+        <h1 className="text-5xl">{word}</h1>
+      ) : (
+        <>
+          <p>ugee bich</p>
+          <input
+            className="bg-black border-white border-[1px]"
+            onChange={(e) => {
+              setClientWord(e.target.value);
+            }}
+          />
+        </>
+      )}
+      <button
+        onClick={() => {
+          findAlphabet({ word: clientWord });
+          setIsFunctionWorking((prev: any) => !prev);
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+};
+
+export default Christmas;
